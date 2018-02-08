82ece6a768adab225dc8290a092d5e433052c0fe
kurinto <aika.miura@gmail.com>
2018-02-08 16:29:33 -0500

    Lesson 11 completed

--- src/org/totalbeginner/tutorial/MyLibrary.java
+++ src/org/totalbeginner/tutorial/MyLibrary.java
@@ -16,4 +16,33 @@ public class MyLibrary {
 	
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public ArrayList<Book> getBooks() {
+		return books;
+	}
+
+	public ArrayList<Person> getPeople() {
+		return people;
+	}
+
+	public void addBook(Book b1) {
+		this.books.add(b1);
+	}
+
+	public void removeBook(Book b1) {
+		this.books.remove(b1);
+	}
+
+	public void addPerson(Person p1) {
+		this.people.add(p1);
+	}
+
+	public void removePerson(Person p1) {
+		this.people.remove(p1);
+	}
+
+	
 }
--- test/org/totalbeginner/tutorial/MyLibraryTest.java
+++ test/org/totalbeginner/tutorial/MyLibraryTest.java
@@ -6,6 +6,12 @@ import junit.framework.TestCase;
 
 public class MyLibraryTest extends TestCase {
 	
+	private Book b1;
+	private Book b2;
+	private Person p1;
+	private Person p2;
+	private MyLibrary ml;
+
 	//test constructor
 	public void testMyLibrary() {
 		MyLibrary ml = new MyLibrary("Test");
@@ -18,4 +24,63 @@ public class MyLibraryTest extends TestCase {
 		
 	}
 
+	//setup
+	
+	public void setup() {
+		b1 = new Book("Book1");
+		b2 = new Book("Book2");
+		
+		p1 = new Person();
+		p2 = new Person();
+		p1.setName("Fred");
+		p2.setName("Sue");
+		
+		ml = new MyLibrary("Test");
+		
+		
+	}
+	
+	public void testAddBook() {
+		//create test objects
+		setup();
+		
+		//test initial size is 0
+		assertEquals(0, ml.getBooks().size());
+		
+		ml.addBook(b1);
+		ml.addBook(b2);
+		assertEquals(2, ml.getBooks().size());
+		assertEquals(0, ml.getBooks().indexOf(b1));
+		assertEquals(1, ml.getBooks().indexOf(b2));
+		
+		ml.removeBook(b1);
+		assertEquals(1, ml.getBooks().size());
+		assertEquals(0, ml.getBooks().indexOf(b2));
+
+		ml.removeBook(b2);
+		assertEquals(0, ml.getBooks().size());
+
+	}
+
+	public void testAddPerson() {
+		//create objects
+		setup();
+		
+		assertEquals(0, ml.getPeople().size());
+
+		ml.addPerson(p1);
+		ml.addPerson(p2);
+		assertEquals(2, ml.getPeople().size());
+		assertEquals(0, ml.getPeople().indexOf(p1));
+		assertEquals(1, ml.getPeople().indexOf(p2));
+		
+		ml.removePerson(p1);
+		assertEquals(1, ml.getPeople().size());
+		assertEquals(0, ml.getPeople().indexOf(p2));
+
+		ml.removePerson(p2);
+		assertEquals(0, ml.getPeople().size());
+
+	}
+	
 }
